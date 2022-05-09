5843ff896ed77a0594f34f0f76ee04994eb9d6f0
Mauro Sanchez Matamoros <maurosanchezmatamoros@gmail.com>
2022-05-09 15:41:46 -0300

    Validacion de stock

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,13 +1,14 @@
 import { useState, useContext } from "react"
 import CartContext from "../../Context/CartContext"
-import { addDoc, collection, Timestamp } from "firebase/firestore"
+import { addDoc, collection, documentId, getDocs, query, Timestamp, where, writeBatch } from "firebase/firestore"
 import { firestoreDb } from "../../services/firebase/index"
 import { LoaderWidget } from "../Loader/Loader"
 import "./Order.css"
+import swal from "sweetalert"
 
 const Order = () => {
 
-    const { cart, getTotal } = useContext(CartContext)
+    const { cart, getTotal, clearCart } = useContext(CartContext)
     const [loading, setLoading] = useState(false)
     const [disabledSubmit, setDisabledSubmit] = useState(false)
     const [orderSubmit, setOrderSubmit] = useState("orderSubmit")
@@ -25,29 +26,55 @@ const Order = () => {
         })
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        addDocToCollection()
-        setLoading(true)
-        setDisabledSubmit(true)
-        setOrderSubmit("")
-    }
-
-    const addDocToCollection = () => {
+    const createOrder = () => {
         const order = {
             buyer: buyer,
             items: cart,
             date: Timestamp.fromDate(new Date()),
             total: getTotal()
         }
-        console.log(order)
 
-        const collectionRef = collection(firestoreDb, "orders")
+        const batch = writeBatch(firestoreDb)
 
-        addDoc(collectionRef, order)
-            .then(response => {console.log(response.id)})
-            .catch(err => {console.log(err)})
-            .finally(() => setLoading(false))
+        const productsIds = order.items.map(prod => prod.id)
+
+        const collectionRef = collection(firestoreDb, "products")
+
+        const outOfStock = []
+
+        getDocs(query(collectionRef, where(documentId(), 'in', productsIds)))
+        .then(response => {
+            response.docs.forEach(doc => {
+                const dataDoc = doc.data()
+                const orderQuantity = cart.find(prod => prod.id === doc.id)?.quantity
+
+                dataDoc.stock >= orderQuantity ?
+                batch.update(doc.ref, { stock: dataDoc.stock - orderQuantity }) :
+                outOfStock.push( {id: doc.id, productName: dataDoc.productName} )
+            })
+        }).then(() => {
+            if (outOfStock.length === 0){
+                const collectionRef = collection(firestoreDb, 'orders')
+                return addDoc(collectionRef, order)}
+            else {return Promise.reject({error: 'OutOfStockError', prods: outOfStock}),
+                swal("Lo siento!", `No hay stock disponible`, "error")}
+            }).then(({id}) => {
+                batch.commit()
+                swal("Felicitaciones!", `Se agregó la orden de compra con el ID: ${id}`, "success")
+                console.log(`Se agregó la orden de compra con el ID: ${id}`)
+                clearCart()
+            }).catch(err => {
+                console.log(err)
+                swal("Lo siento!", `No se ha podido generar la orden de compra`, "error")
+            }).finally(() => setLoading(false))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        createOrder()
+        setLoading(true)
+        setDisabledSubmit(true)
+        setOrderSubmit("")
     }
 
     return(
